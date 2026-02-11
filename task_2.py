a50495730ef2e309f03ebf6a289b60646309f86a
fld1t <roma.surnin.07@mail.ru>
2026-02-12 04:15:15 +0700

    2

---
+++ task_2.py
@@ -0,0 +1,35 @@
+def filter_data():
+    students_dict = {}
+
+    with open("resource/students.txt", "r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    result_file = open("result.txt", "w", encoding="utf-8")
+
+    for line in lines:
+        line = line.strip()
+        try:
+            name, rates = line.split(":")
+
+        except ValueError:
+            continue
+
+        grades_list = rates.split(",")
+
+        value = 0
+
+        for rate in grades_list:
+            value += int(rate)
+
+        average_value = value / len(grades_list)
+
+        students_dict[name] = round(average_value, 2)
+
+        if average_value > 4.0:
+            result_file.write(f"{name}: {average_value}\n")
+
+    best_student = max(students_dict,key=students_dict.get)
+    best_score = students_dict[best_student]
+    print(f"Лучший студент: {best_student}: {best_score}")
+
+filter_data()
